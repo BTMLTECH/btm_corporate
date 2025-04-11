1700efb31f7d54609c07c59dcef26e88ba28aae2
TobiLight <jgetitdonefast@gmail.com>
2025-04-11 14:24:50 +0100

    fix: FAQ Page

---
+++ src/routes/(root)/faqs/+page.svelte
@@ -0,0 +1,112 @@
+<section id="main-content">
+  <div class="w-10/12 md:w-9/12 mx-auto mt-20">
+    <h2 class="text-4xl font-semibold pt-20 mb-5">
+      Frequently asked questions
+    </h2>
+    <dl class="mt-10 space-y-6 divide-y divide-gray-900/10">
+      <div class="pt-6">
+        <dt>
+          <button
+            type="button"
+            class="flex w-full items-start justify-between text-left text-gray-900"
+            aria-controls="faq-0"
+            aria-expanded="true"
+          >
+            <span class="text-base font-semibold leading-7"
+              >What services does your TMC offer?</span
+            >
+            <span class="ml-6 flex h-7 items-center">
+              <svg
+                class="h-6 w-6 hidden"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M12 6v12m6-6H6"
+                >
+                </path>
+              </svg>
+              <svg
+                class="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M18 12H6"
+                ></path>
+              </svg>
+            </span>
+          </button>
+        </dt>
+        <dd class="mt-2 pr-12" id="faq-0">
+          <p class="text-base leading-7 text-gray-600">
+            We provide flight and hotel bookings, itinerary management, expense tracking, travel policy compliance, visa assistance, risk management, and 24/7 travel support.
+          </p>
+        </dd>
+      </div>
+      <div class="pt-6">
+        <dt>
+          <button
+            type="button"
+            class="flex w-full items-start justify-between text-left text-gray-900"
+            aria-controls="faq-1"
+            aria-expanded="false"
+          >
+            <span class="text-base font-semibold leading-7"
+              >How is a TMC different from a regular travel agency?</span
+            >
+            <span class="ml-6 flex h-7 items-center">
+              <svg
+                class="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M12 6v12m6-6H6"
+                >
+                </path>
+              </svg>
+              <svg
+                class="hidden h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M18 12H6"
+                ></path>
+              </svg>
+            </span>
+          </button>
+        </dt>
+        <dd class="mt-2 pr-12" id="faq-1" style="display: none;">
+          <p class="text-base leading-7 text-gray-600">
+            Unlike standard travel agencies, a TMC specializes in corporate travel with a focus on cost control, policy compliance, reporting, and traveler safety.
+          </p>
+        </dd>
+      </div>
+      
+    </dl>
+  </div>
+</section>
+
+<div class="border-t border-gray-200 mt-16"></div>
