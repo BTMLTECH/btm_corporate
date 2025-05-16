3cdbb1d0a1f44839eeb25a5c6673ec1d7f44a919
TobiLight <jgetitdonefast@gmail.com>
2025-05-16 10:50:26 +0100

    chore: Fix content and broken links

--- src/lib/components/shared/Footer.svelte
+++ src/lib/components/shared/Footer.svelte
@@ -82,12 +82,12 @@
         <h4 class="text-lg text-white font-medium mb-4">Legal</h4>
         <ul class="text-sm transition-all duration-500">
           <li class="mb-3">
-            <a href="/terms" class="text-gray-300 hover:text-white"
+            <a href="/maintenance" class="text-gray-300 hover:text-white"
               >Terms & Conditions</a
             >
           </li>
           <li class="mb-3">
-            <a href="/privacy" class="text-gray-300 hover:text-white"
+            <a href="/maintenance" class="text-gray-300 hover:text-white"
               >Privacy Policy</a
             >
           </li>
